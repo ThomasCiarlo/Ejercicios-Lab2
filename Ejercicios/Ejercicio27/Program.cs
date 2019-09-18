c979ddb8046125fdf689832c91207804fe9e9d83
ThomasCiarlo <thomaciarlo18@gmail.com>
2019-09-18 13:36:40 -0300

    18-9-201

--- Ejercicios/Ejercicio27/Program.cs
+++ Ejercicios/Ejercicio27/Program.cs
@@ -10,60 +10,109 @@ namespace Ejercicio27
     {
         static void Main(string[] args)
         {
-            int numero;
+            
             Random num = new Random();
 
-            List<int> numerosList = new List<int>();
+            #region conListas
+            /* List<int> numerosList = new List<int>();
+             List<int> NumeroPositivo = new List<int>();
+             List<int> NumeroNegativo = new List<int>();
+
+             for (int i = 0; i < 20; i++) {               
+                 numerosList.Add(num.Next(-100, 100));
+             }
+
+             for (int i = 0; i < 20; i++) {
+
+                 if (numerosList[i] < 0) {
+
+                     NumeroNegativo.Add(numerosList[i]);
+
+                 }
+
+             }
+
+             for (int i = 0; i < 20; i++)
+             {
+
+                 if (numerosList[i] >= 0)
+                 {
+
+                     NumeroPositivo.Add(numerosList[i]);
+
+                 }
+
+             }
+
+
+             for (int i = 0; i < 20; i++) {
+                 Console.Write(numerosList[i]+",");
+             }
+
+             NumeroPositivo.Sort();
+             NumeroPositivo.Reverse();
+             Console.WriteLine("\n Numeros Positivos: ");
+             foreach (int c in NumeroPositivo) {
+
+                 Console.Write(c + ",");
+             }
+
+             NumeroNegativo.Sort();
+             Console.WriteLine("\nNumeros Negativos: ");
+             foreach (int c in NumeroNegativo)
+             {
+
+                 Console.Write(c+ ",");
+             }
+             */
+            #endregion
+
+            Stack<int> numerosLista = new Stack<int>();
+
+
             List<int> NumeroPositivo = new List<int>();
             List<int> NumeroNegativo = new List<int>();
 
             for (int i = 0; i < 20; i++) {
-                numero = num.Next(-100, 100);
-                numerosList.Add(numero);
-            }
 
-            for (int i = 0; i < 20; i++) {
-
-                if (numerosList[i] < 0) {
-
-                    NumeroNegativo.Add(numerosList[i]);
-
-                }
+                numerosLista.Push(num.Next(-100, 100));
 
             }
 
-            for (int i = 0; i < 20; i++)
-            {
+            foreach (int c in numerosLista) {
 
-                if (numerosList[i] >= 0)
+                Console.Write(c + ", ");
+
+                if (c < 0)
                 {
-
-                    NumeroPositivo.Add(numerosList[i]);
-
+                    NumeroNegativo.Add(c);
                 }
-
+                else {
+                    NumeroPositivo.Add(c);
+                }
             }
-
-
-            for (int i = 0; i < 20; i++) {
-                Console.Write(numerosList[i]+",");
-            }
-
+            Console.WriteLine("\nNumeros Positivos: ");
             NumeroPositivo.Sort();
             NumeroPositivo.Reverse();
-            Console.WriteLine("\n Numeros Positivos: ");
             foreach (int c in NumeroPositivo) {
-
-                Console.Write(c + ",");
+                
+                Console.Write(c + ", ");
             }
 
+
+            Console.WriteLine("\nNumeros negativos: ");
             NumeroNegativo.Sort();
-            Console.WriteLine("\nNumeros Negativos: ");
             foreach (int c in NumeroNegativo)
             {
-
-                Console.Write(c+ ",");
+                
+                Console.Write(c + ", ");
             }
+
+
+
+
+
+
 
 
             Console.ReadKey();
