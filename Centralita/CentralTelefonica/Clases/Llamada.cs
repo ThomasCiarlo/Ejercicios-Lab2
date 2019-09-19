bae65acb256a143ff70f9b446f95817ac12c2eac
Thomas Ciarlo <40859090+ThomasCiarlo@users.noreply.github.com>
2019-09-19 13:20:48 -0300

    Add files via upload

---
+++ Centralita/CentralTelefonica/Clases/Llamada.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        public float Duracion { get { return this.duracion; } }
+        public string NroDestino { get { return this.nroDestino; } }
+        public string NroOrigen {get { return this.nroOrigen; } }
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+
+        public string Mostar()
+        {
+            StringBuilder texto = new StringBuilder($"Duracion: {Duracion}, Numero de destino: {NroDestino}, Numero de origen: {NroOrigen}");
+
+            return texto.ToString();
+        }
+
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+
+        }
+    }
+}
---
+++ Centralita/CentralTelefonica/Clases/Local.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Local : Llamada
+    {
+        protected float costo;
+        public float CostoLlamada { get { return this.CalcularCosto(); } }
+
+       private float CalcularCosto()
+        {
+            float precio = Duracion * costo;
+
+            return precio;
+        }
+
+        public Local(Llamada llamada, float costo) : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.costo = costo;
+        }
+
+        public Local(float duracion,string origen, string destino, float costo): base(duracion,destino,origen)
+        {
+            this.costo = costo; 
+                       
+        }
+        
+    }
+}
