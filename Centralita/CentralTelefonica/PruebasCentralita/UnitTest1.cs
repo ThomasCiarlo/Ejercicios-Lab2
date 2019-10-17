bc57e6146713d513720edf993baa51cee964c00a
alumno <thomasciarlo18@gmail.com>
2019-10-17 19:23:43 -0300

    Centralita al dia

--- Centralita/CentralTelefonica/PruebasCentralita/UnitTest1.cs
+++ Centralita/CentralTelefonica/PruebasCentralita/UnitTest1.cs
@@ -4,15 +4,62 @@ using Clases;
 
 namespace PruebasCentralita
 {
-  [TestClass]
-  public class UnitTest1
-  {
-    [TestMethod]
-    public void CentralitaInstanciada()
+    [TestClass]
+    public class UnitTest1
     {
-      Centralita central = new Centralita();
+        [TestMethod]
+        public void CentralitaInstanciada()
+        {
+            Centralita central = new Centralita();
 
-      Assert.AreSame(null, central);
+            Assert.IsTrue(central.Llamadas != null);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void excepcionLlamadasLocal()
+        {
+            Centralita central = new Centralita();
+            Local l1 = new Local("123", 10, "1234", 15);
+
+            central += l1;
+
+            Local l2 = new Local("123", 10, "1234", 15);
+
+            central += l2;
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void excepcionLlamadasProvincial()
+        {
+            Centralita central = new Centralita();
+            Provincial l1 = new Provincial("123", Provincial.Franja.Franja_1, 15, "1234");
+
+            central += l1;
+
+            Provincial l2 = new Provincial("123", Provincial.Franja.Franja_1, 15, "1234");
+            central += l2;
+
+        }
+
+        [TestMethod]
+        public void ComprarLlamadas()
+        {
+            Provincial l1 = new Provincial("123", Provincial.Franja.Franja_1, 15, "1234");
+            Provincial l2 = new Provincial("123", Provincial.Franja.Franja_1, 15, "1234");
+
+            Local l3 = new Local("123", 10, "1234", 15);
+            Local l4 = new Local("123", 10, "1234", 15);
+
+            Assert.IsFalse(l1 == l3);
+            Assert.IsFalse(l2 == l4);
+            Assert.IsTrue(l1 == l2);
+            Assert.IsTrue(l3 == l4);
+
+
+
+        }
     }
-  }
 }
